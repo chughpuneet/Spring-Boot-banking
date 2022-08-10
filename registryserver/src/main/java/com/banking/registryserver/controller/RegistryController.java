6c33ab017a648a844771b19c20da3c90d7691419
Puneet.Chugh <chughpuneet@gmail.com>
2022-08-10 22:57:54 +1000

    Added RegistryController and updated the registry properties

---
+++ registryserver/src/main/java/com/banking/registryserver/controller/RegistryController.java
@@ -0,0 +1,34 @@
+package com.banking.registryserver.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@RestController
+@RequestMapping("registry")
+public class RegistryController {
+
+    @Autowired
+    Environment env;
+
+    @GetMapping("/config")
+    public Map getConfig(){
+        Map<String, Object> map = new HashMap<>();
+        for(Iterator it = ((AbstractEnvironment) env).getPropertySources().iterator(); it.hasNext(); ) {
+            PropertySource propertySource = (PropertySource) it.next();
+            if (propertySource instanceof MapPropertySource) {
+                map.putAll(((MapPropertySource) propertySource).getSource());
+            }
+        }
+        return map;
+    }
+}
